a56f8e133b2d9eb32ff40fbe56df80a4c8950f26
Jet <jwoot001@gmail.com>
2021-09-05 00:49:42 +0800

    edit !command default to 1

--- src/Commands/axs.js
+++ src/Commands/axs.js
@@ -10,7 +10,13 @@ module.exports = new Command({
 	async run(message, args, client) {
 
 		if (isNaN(parseFloat(args[1]))) {
-			return message.channel.send("Invalid amount. Type `!axs <amount>` or `!axs <amount> @ <price>`.")
+			
+			fetch('https://api.coingecko.com/api/v3/simple/price?ids=axie-infinity&vs_currencies=php')
+			.then(res => res.json())
+			.then(body => {
+				let x = (body['axie-infinity'].php * args[1]).toFixed(2);
+				message.reply(`\`AXS\` **${args[1]}** => \`PHP\` **${x}**`);
+			});
 
 		} else if (args[2] === "@" && !args[3]) {
 			return message.channel.send("Invalid command. Type `!axs <amount> <@> <price>`.")
