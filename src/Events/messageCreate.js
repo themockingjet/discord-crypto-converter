5f3eed5bb8f481a440294ee93c9e1501b2f0aac1
Jet <jwoot001@gmail.com>
2021-09-10 08:14:46 +0800

    Case sensitive

--- src/Events/messageCreate.js
+++ src/Events/messageCreate.js
@@ -10,7 +10,7 @@ module.exports = new Event("messageCreate", (client, message) => {
 
 	const args = message.content.substring(client.prefix.length).split(/ +/);
 
-	const command = client.commands.find(cmd => cmd.name == args[0]);
+	const command = client.commands.find(cmd => cmd.name == args[0].toLowerCase());
 
 	if (!command) return message.reply(`${args[0]} is not a valid command!`);
 
